06f34a69d22635c01f4e3d5ec163d8e99c108a3e
Cherpatel <zakrikz@gmail.com>
2024-06-17 04:01:20 +0300

    minor fixes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,7 @@
-import { useState } from "react";
-
-// import styles from "./App.module.css";
+import styles from "./App.module.css";
 
 export default function App() {
-    const [counter, setCounter] = useState<number>(0);
-
     return (
-        <div>
-            <button
-                onClick={ () => setCounter(counter + 1) }
-            ></button>
-
-            <span>counter = { counter }</span>
-        </div>
+        <div className={ styles.app }></div>
     );
 }
