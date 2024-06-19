7497e12996dd8c1a56433dfc10d92ef749cb4951
Cherpatel <zakrikz@gmail.com>
2024-06-19 07:34:37 +0300

    add root class name for css modules

--- config/build/buildLoaders/scssLoader.ts
+++ config/build/buildLoaders/scssLoader.ts
@@ -1,3 +1,6 @@
+import crypto from "crypto";
+import path from "path";
+
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 import type { BuildOptions } from "@config/build/types/types";
@@ -12,7 +15,17 @@ export function scssLoader(options: BuildOptions): RuleSetRule {
             importLoaders: 1,
             modules: {
                 exportLocalsConvention: "camel-case-only",
-                localIdentName: "[folder]__[local]_[hash:base64:5]",
+                getLocalIdent: (context, localIdentName, localName) => {
+                    const resourcePath = context.resourcePath;
+                    const fileName = path.basename(resourcePath, path.extname(resourcePath)).replace(".module", "");
+                    const hash = crypto.createHash("md5").update(context.resourcePath).digest("hex").slice(0, 5);
+
+                    if (localName === "root") {
+                        return `${ fileName }_${ hash }`;
+                    }
+
+                    return `${ fileName }_${ localName }_${ hash }`;
+                },
                 namedExport: false,
             },
             sourceMap: true,
