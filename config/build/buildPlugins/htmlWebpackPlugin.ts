8ac283db5292aff10d30cf590aeef3b496e1a391
Cherpatel <zakrikz@gmail.com>
2024-06-24 22:44:15 +0300

    update html webpack plugin options for routing

--- config/build/buildPlugins/htmlWebpackPlugin.ts
+++ config/build/buildPlugins/htmlWebpackPlugin.ts
@@ -16,6 +16,7 @@ export function htmlWebpackPluginInstance(options: BuildOptions) {
 
     return new HtmlWebpackPlugin({
         ...(isFaviconExists && { favicon }),
+        publicPath: "/",
         template: html,
     });
 }
